6607bb21e3d21659a8fc233f4e12ea123580af44
The Maximalist <hello@themaximalist.com>
2023-04-15 12:08:34 -0700

    added resized image

--- src/controllers/art.js
+++ src/controllers/art.js
@@ -1,5 +1,6 @@
 const log = require("debug")("ia:controllers:art");
 
+const sharp = require("sharp");
 const Game = require("../models/game");
 const GenerateGameArt = require("../services/GenerateGameArt");
 
@@ -32,18 +33,33 @@ async function generate(req, res) {
 async function get(req, res) {
     try {
         const { slug } = req.params;
+        let size = Number(req.query.size) || 512;
         const game = await Game.findOne({ where: { slug } });
         if (!game) throw new Error(`game with slug ${slug} not found`);
 
+        if (size > 512) size = 512;
+        if (size < 64) size = 64;
 
         if (game.image_data) {
-            const contentType = (game.image_model_name === "replicate" ? "image/jpeg" : "image/png");
+            const contentType = (game.image_prompt_model === "replicate" ? "image/jpeg" : "image/png");
+
+            let resizedImage;
+            switch (game.image_prompt_model) {
+                case "replicate":
+                    resizedImage = await sharp(game.image_data).resize(size, size).jpeg().toBuffer();
+                    break;
+                case "stability":
+                    resizedImage = await sharp(game.image_data).resize(size, size).png().toBuffer();
+                    break;
+                default:
+                    throw new Error(`unknown image model name ${game.image_prompt_model}`);
+            }
 
             res.writeHead(200, {
                 'Content-Type': contentType
             });
 
-            res.end(game.image_data, "binary");
+            res.end(resizedImage, "binary");
         } else {
             throw new Error(`game with slug ${slug} has no art`)
         }
