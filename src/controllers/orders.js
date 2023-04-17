63dd1b48021e6835c5887868c06ad24724ed46b6
The Maximalist <hello@themaximalist.com>
2023-04-17 15:48:14 -0700

    updated stripe key variable name

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -1,5 +1,5 @@
 const log = require("debug")("ia:controllers:orders");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_API_KEY);
 
 const Order = require("../models/order");
 
