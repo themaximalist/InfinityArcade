725f2bb727c4bb26696be5b6431a4f89ed7eaf31
The Maximalist <hello@themaximalist.com>
2023-04-21 12:43:30 -0700

    debugging slow admin

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -4,7 +4,7 @@ const stats = require("../services/stats");
 async function index(req, res) {
     const dailySummary = await stats.getDailySummary();
     const totalCounts = await stats.getTotalCounts();
-    const topSummary = await stats.getTopSummary();
+    const topSummary = { games: [] }; //await stats.getTopSummary();
     return res.render("admin/index", { dailySummary, totalCounts, topSummary });
 }
 
