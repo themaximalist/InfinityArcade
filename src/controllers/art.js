a69c0c7fa573e6f32e4d30dbf91f25282bca5f39
The Maximalist <hello@themaximalist.com>
2023-04-25 22:13:14 -0700

    re-enable gpt4 for pro customers

--- src/controllers/art.js
+++ src/controllers/art.js
@@ -13,8 +13,7 @@ async function generate(req, res) {
 
             if (game.image_data) return res.redirect(`/api/game/${game.slug}/art`);
 
-            // let model = (req.user ? req.user.model : process.env.LLM_MODEL);
-            let model = process.env.LLM_MODEL; // all out of gpt4 credits for the month :(
+            let model = (req.user ? req.user.model : process.env.LLM_MODEL);
 
             const art = await GenerateGameArt(game.llm_fields, model);
             if (!art) throw new Error(`art not generated`);
