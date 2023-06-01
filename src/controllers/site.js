fa3e6883e47ceff96f65ce6a2ed89debf6b31457
The Maximalist <hello@themaximalist.com>
2023-06-01 16:05:37 -0700

    added article

--- src/controllers/site.js
+++ src/controllers/site.js
@@ -11,10 +11,16 @@ function articles(req, res) {
 }
 
 function article(req, res) {
+    // TODO: unsustainable
     if (req.params.slug === "how-to-create-your-own-text-adventure-game") {
         return res.render("articles/how-to-create-your-own-text-adventure-game", {
             title: "How to create your own text adventure game",
             description: "A step-by-step guide to creating your own text adventure game with Infinity Arcade in seconds."
+        });
+    } else if (req.params.slug === "generative-video-game-soundtracks") {
+        return res.render("articles/generative-video-game-soundtracks", {
+            title: "Generative Video Game Soundtracks",
+            description: "How Infinity Arcade is using generative music to create video game soundtracks."
         });
     }
     res.status(404).send("Not found");
