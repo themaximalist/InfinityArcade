90a407011897f780db677f3d64b553259118bf6c
The Maximalist <hello@themaximalist.com>
2023-12-30 17:00:28 -0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,17 @@
-git pull
-npm install
-pm2 restart src/index.js --name infinityarcade.com
+#!/bin/bash
+
+APP_NAME="infinityarcade.com"
+
+UPDATES=$(git pull)
+if [[ $UPDATES != *"Already up to date"* ]]; then
+  npm install
+
+  if pm2 list | grep -q $APP_NAME; then
+    pm2 restart $APP_NAME
+  else
+    pm2 start src/index.js --name $APP_NAME
+  fi
+else
+  echo "No updates found. Nothing to do."
+fi
+
