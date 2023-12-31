60b097e2844333d77e3d048ea2a1c8b4da696e7f
The Maximalist <hello@themaximalist.com>
2023-12-30 18:04:13 -0800

    fix streaming json parsing

--- src/services/parseTokenStream.js
+++ src/services/parseTokenStream.js
@@ -1,42 +1,53 @@
 async function* parseTokenStream(response) {
-
     let last_token = null;
     let option = null;
+    let accumulatedData = ""; // Initialize an empty string to accumulate data
+
     for await (const chunk of response.data) {
-        const lines = chunk
-            .toString("utf8")
-            .split("\n")
-            .filter((line) => line.trim().startsWith("data: "));
+        accumulatedData += chunk.toString("utf8"); // Accumulate chunks
 
-        for (const line of lines) {
-            const message = line.replace(/^data: /, "");
-            if (message === "[DONE]") {
-                return;
-            }
-
-            const json = JSON.parse(message);
-            const token = json.choices[0].delta.content;
-            if (!token) continue;
-
-            if (last_token && last_token.endsWith("\n") && token.match(/^\d$/)) {
-                option = token;
-                yield { type: `option${option}`, content: token };
-                last_token = token;
-                continue;
-            }
-
-            if (option) {
-                if (token == "\n") {
-                    option = null;
-                } else {
-                    yield { type: `option${option}`, content: token };
+        // Split accumulated data by newlines and process complete messages
+        let lines = accumulatedData.split("\n");
+        for (let i = 0; i < lines.length - 1; i++) { // Iterate until the second-to-last element
+            const line = lines[i].trim();
+            if (line.startsWith("data: ")) {
+                const message = line.replace(/^data: /, "");
+                if (message === "[DONE]") {
+                    return;
                 }
-            } else {
-                yield { type: "content", content: token };
-            }
 
-            last_token = token;
+                try {
+                    const json = JSON.parse(message);
+                    const token = json.choices[0].delta.content;
+                    if (!token) continue;
+
+                    if (last_token && last_token.endsWith("\n") && token.match(/^\d$/)) {
+                        option = token;
+                        yield { type: `option${option}`, content: token };
+                        last_token = token;
+                        continue;
+                    }
+
+                    if (option) {
+                        if (token == "\n") {
+                            option = null;
+                        } else {
+                            yield { type: `option${option}`, content: token };
+                        }
+                    } else {
+                        yield { type: "content", content: token };
+                    }
+
+                    last_token = token;
+                } catch (e) {
+                    // If JSON parsing fails, accumulate the data until a complete message is formed
+                    continue;
+                }
+            }
         }
+
+        // Preserve incomplete data for the next iteration
+        accumulatedData = lines[lines.length - 1];
     }
 }
 
