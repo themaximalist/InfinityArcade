248df315afe2b5957521a4a7b3312e173cb107dd
The Maximalist <hello@themaximalist.com>
2023-12-30 22:09:15 -0800

    save image when generated as file

--- src/controllers/art.js
+++ src/controllers/art.js
@@ -1,6 +1,8 @@
 const log = require("debug")("ia:controllers:art");
 
+const fs = require("fs");
 const sharp = require("sharp");
+
 const Game = require("../models/game");
 const GenerateGameArt = require("../services/GenerateGameArt");
 
@@ -11,12 +13,30 @@ async function generate(req, res) {
             const game = await Game.findOne({ where: { id: game_id } });
             if (!game) throw new Error(`game with id ${game_id} not found`);
 
-            if (game.image_data) return res.redirect(`/images/art/${game.id}.png`);
+            const filename = `./public/images/art/${game.id}.png`;
+            const filename_256 = `./public/images/art/${game.id}-256.png`;
+            const filename_50 = `./public/images/art/${game.id}-50.png`;
+
+            if (fs.existsSync(filename)) {
+                return res.redirect(`/images/art/${game.id}.png`);
+            }
 
             let model = (req.user ? req.user.model : process.env.AI_MODEL);
 
             const art = await GenerateGameArt(game.llm_fields, model);
             if (!art) throw new Error(`art not generated`);
+
+            if (art.image_data) {
+
+                const image = await sharp(art.image_data).png();
+
+                await image.toFile(filename);
+                await image.resize(256, 256).toFile(filename_256);
+                await image.resize(50, 50).toFile(filename_50);
+
+                delete art.image_data;
+            }
+
 
             await Game.update(art, { where: { id: game.id } });
 
